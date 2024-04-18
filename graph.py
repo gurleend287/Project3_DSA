d6be951b7e956eb731ae10365dcf82cf5d362158
Gurleen Dhillon <gurleend04@gmail.com>
2024-04-18 15:41:51 -0400

    implemented add_node function

--- graph.py
+++ graph.py
@@ -3,12 +3,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class Graph:
     def __init__(self, threshold_map):
-        self.adjacency_list = {}
+        self.adj_list = {}
         self.threshold_map = threshold_map
+        self.num1 = 0
     
     # helper function
     # returns similarity score used to determine if two nodes should be connected
-    def find_similarity(node1: Node, node2: Node):
+    def find_similarity(self, node1: Node, node2: Node):
         features1 = [node1.danceability, node1.energy, node1.valence, node1.tempo]
         features2 = [node2.danceability, node2.energy, node2.valence, node2.tempo]
         
@@ -18,5 +19,25 @@ class Graph:
         
         return cosine_similarity(features1, features2)[0][0]
     
-    # def add_node(self, node: Node, cols: list[str]):
+    # add edge between nodes if similarity score theshold is met
+    # similary score is weight of edge
+    def add_edge(self, node1: Node, node2: Node):
+        sim_score = self.find_similarity(node1, node2)
+        # getting high similarity scores for most nodes - 0.999985 is good threshold for now
+        if sim_score >= 0.999985:
+            key1 = (node1.track_id, node1)
+            key2 = (node2.track_id, node2)
+            self.adj_list[key1].append((key2, sim_score))
+            self.adj_list[key2].append((key1, sim_score))
+    
+    # add node to graph based on mood theshold criteria and connect edges
+    def add_node(self, node: Node, cols: list[str]):
+        for col in cols:
+            if getattr(node, col) < self.threshold_map[col][0] or getattr(node, col) > self.threshold_map[col][1]:
+                # print("false")
+                return # node not added
         
+        # num1 = num1+1
+        key = (node.track_id, node)
+        if node.track_id not in self.adj_list:
+            self.adj_list[key] = []
--- main.py
+++ main.py
@@ -54,38 +54,72 @@ df2 = df[relevant_cols].copy()
 # may need to broaden ranges/overlap if not enough songs meet criteria
 criteria = ['danceability', 'energy', 'valence', 'tempo']
 ranges_map = {}
+num_ranges = 5
 
-for col in criteria:
+for col in criteria:    
     min  = df2[col].min()
     max = df2[col].max()
-    step = (max - min) / 10
+    step = (max - min) / num_ranges
     
     ranges = {}
-    for i in range(1,11):
+    for i in range(1,1 + num_ranges):
         lower = min + (i - 1) * step
-        upper = max + i * step
+        upper = min + i * step
         ranges[i] = (lower, upper)
         
     ranges_map[col] = ranges
     
 # get user input for mood
-mood_input = input("Rate your mood (1-10): ")
+mood_input = input("Rate your mood (1-5): ")
 
 # set thresholds based on chosen mood
 threshold_map = {}
 for key, ranges in ranges_map.items():
     threshold_map[key] = ranges.get(int(mood_input))
-
-print(threshold_map)
     
-# test node class
-first = df2.iloc[0].to_dict()
-node1 = Node(**first)
-second = df2.iloc[1].to_dict()
-node2 = Node(**second)
+# # test node class
+# first = df2.iloc[67750].to_dict()
+# node1 = Node(**first)
+# second = df2.iloc[2].to_dict()
+# node2 = Node(**second)
+
+# print(node1.__repr__())
+# print(node2.__repr__())
 
 # # test graph class
-# adj_list = Graph(threshold_map)
-# cols = ['valence', 'energy']
+# temp = 'valence'
+# print(threshold_map['valence'])
+# print(getattr(node1, 'valence'))
+# print(getattr(node1, temp) < threshold_map[temp][0] or getattr(node1, temp) > threshold_map[temp][1])
+
+graph = Graph(threshold_map)
+
+
+# print(adj_list.find_similarity(node1, node2))
 # adj_list.add_node(node1, cols)
 # print(adj_list)
+num1 = 0
+
+# parse through first half of dataset
+# add nodes based on tempo and valence only
+cols1 = ['valence', 'tempo']
+for i, row in df2.iloc[:len(df2)//2].iterrows():
+    num1 = num1 + 1
+    curr_node = Node(**row.to_dict())
+    graph.add_node(curr_node, cols1)
+    
+    # for existing_id, adj_nodes in graph.adj_list:
+    #     existing_node = existing
+    #     graph.add_edge(curr_node, existing_node)
+
+# parse through second half of dataset
+# add nodes based on all 4 criteria
+for i, row in df2.iloc[len(df2)//2:].iterrows():
+    num1 = num1 + 1
+    curr_node = Node(**row.to_dict())
+    graph.add_node(curr_node, criteria)
+
+
+print(num1)
+print(len(graph.adj_list))
+
